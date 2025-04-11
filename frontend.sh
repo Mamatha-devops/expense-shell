253570dcfb5597d4a6568adbf506af49d83b786b
Mamatha-devops <mammu.vangeti@gmail.com>
2025-04-11 06:09:14 -0400

    updated the repo

---
+++ frontend.sh
@@ -0,0 +1,7 @@
+#!bin/bash  
+dnf install nginx -y  
+systemctl enable nginx
+systemctl start nginx
+rm -rf /usr/share/nginx/html/* 
+curl -o /tmp/frontend.zip https://expense-web-app.s3.amazonaws.com/frontend.zip
+unzip /tmp/frontend.zip -d /usr/share/nginx/html
