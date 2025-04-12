efdd1135167ba381a122e3f1ea4dad574fcc4553
Mamatha-devops <mammu.vangeti@gmail.com>
2025-04-12 01:37:33 -0400

    updated the repo

--- frontend.sh
+++ frontend.sh
@@ -1,10 +1,10 @@
 #!bin/bash  
-set-hostname frontend
+#set-hostname frontend
 dnf install nginx -y  
 systemctl enable nginx
 systemctl start nginx
 rm -rf /usr/share/nginx/html/* 
 curl -o /tmp/frontend.zip https://expense-web-app.s3.amazonaws.com/frontend.zip
 unzip /tmp/frontend.zip -d /usr/share/nginx/html
-# systemctl restart nginx
+# systemctl restart ngi
 systemctl restart nginx
